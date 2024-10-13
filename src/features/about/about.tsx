bc2ea719969e462727e782edc3dfbcca9438b99a
Soumya Pal <soumyapal.774@gmail.com>
2024-10-13 22:30:18 +0530

    images added 2

--- src/features/about/about.tsx
+++ src/features/about/about.tsx
@@ -58,25 +58,27 @@ export function About() {
   ));
 
   return (
-    <Container size="lg" py="xl">
-      <Group justify="center">
-        <Badge variant="filled" size="lg">
-          Best company ever
-        </Badge>
-      </Group>
+    <div className={classes.bgimage}>
+      <Container size="lg" py="xl">
+        <Group justify="center">
+          <Badge variant="filled" size="lg">
+            Best company ever
+          </Badge>
+        </Group>
 
-      <Title order={2} className={classes.title} ta="center" mt="sm">
-        Integrate effortlessly with any technology stack
-      </Title>
+        <Title order={2} className={classes.title} ta="center" mt="sm">
+          Integrate effortlessly with any technology stack
+        </Title>
 
-      <Text c="dimmed" className={classes.description} ta="center" mt="md">
-        Every once in a while, you’ll see a Golbat that’s missing some fangs.
-        This happens when hunger drives it to try biting a Steel-type Pokémon.
-      </Text>
+        <Text className={classes.description} ta="center" mt="md">
+          Every once in a while, you’ll see a Golbat that’s missing some fangs.
+          This happens when hunger drives it to try biting a Steel-type Pokémon.
+        </Text>
 
-      <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
-        {features}
-      </SimpleGrid>
-    </Container>
+        <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
+          {features}
+        </SimpleGrid>
+      </Container>
+    </div>
   );
 }
--- src/features/faq/faq.tsx
+++ src/features/faq/faq.tsx
@@ -1,5 +1,18 @@
-import { Container, Title, Accordion, Image, Flex } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Accordion,
+  Image,
+  Flex,
+  Button,
+  Group,
+  List,
+  rem,
+  ThemeIcon,
+  Text,
+} from "@mantine/core";
 import classes from "./faq.module.css";
+import { IconCheck } from "@tabler/icons-react";
 
 const placeholder =
   "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.";
@@ -13,109 +26,161 @@ const images = [
 ];
 export function Faq() {
   return (
-    <Container size="sm" className={classes.wrapper}>
-      <Title ta="center" className={classes.title}>
-        Frequently Asked Questions
-      </Title>
+    <>
+      <Container size="md">
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              A <span className={classes.highlight}>modern</span> React <br />{" "}
+              components library
+            </Title>
+            <Text c="dimmed" mt="md">
+              Build fully functional accessible web applications faster than
+              ever – Mantine includes more than 120 customizable components and
+              hooks to cover you in any situation
+            </Text>
 
-      <Accordion variant="separated">
-        <Accordion.Item className={classes.item} value="reset-password">
-          <Accordion.Control>How can I reset my password?</Accordion.Control>
-          <Accordion.Panel>
-            {" "}
-            <Flex>
-              {placeholder}
-              <Image
-                className={classes.faqImage}
-                m={10}
-                ml={30}
-                src={images[1]}
-                height={180}
-                w={180}
-              />
-            </Flex>
-          </Accordion.Panel>
-        </Accordion.Item>
+            <List
+              mt={30}
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  <IconCheck
+                    style={{ width: rem(12), height: rem(12) }}
+                    stroke={1.5}
+                  />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
+                <b>TypeScript based</b> – build type safe applications, all
+                components and hooks export types
+              </List.Item>
+              <List.Item>
+                <b>Free and open source</b> – all packages have MIT license, you
+                can use Mantine in any project
+              </List.Item>
+              <List.Item>
+                <b>No annoying focus ring</b> – focus ring will appear only when
+                user navigates with keyboard
+              </List.Item>
+            </List>
+          </div>
+          <Image
+            src="https://ui.mantine.dev/_next/static/media/image.9a65bd94.svg"
+            className={classes.image}
+          />
+        </div>
+      </Container>
+      <div className={classes.bgimg}>
+        <Container size="lg" className={classes.wrapper}>
+          <Title ta="center" className={classes.title}>
+            Frequently Asked Questions
+          </Title>
 
-        <Accordion.Item className={classes.item} value="another-account">
-          <Accordion.Control>
-            Can I create more that one account?
-          </Accordion.Control>
-          <Accordion.Panel>
-            <Flex>
-              {placeholder}
-              <Image
-                className={classes.faqImage}
-                m={10}
-                ml={30}
-                src={images[0]}
-                height={180}
-                w={180}
-              />
-            </Flex>
-          </Accordion.Panel>
-        </Accordion.Item>
+          <Accordion variant="separated">
+            <Accordion.Item className={classes.item} value="reset-password">
+              <Accordion.Control>
+                How can I reset my password?
+              </Accordion.Control>
+              <Accordion.Panel>
+                {" "}
+                <Flex>
+                  {placeholder}
+                  <Image
+                    className={classes.faqImage}
+                    m={10}
+                    ml={30}
+                    src={images[1]}
+                    height={220}
+                    w={220}
+                  />
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="newsletter">
-          <Accordion.Control>
-            How can I subscribe to monthly newsletter?
-          </Accordion.Control>
-          <Accordion.Panel>
-            {" "}
-            <Flex>
-              {placeholder}
-              <Image
-                className={classes.faqImage}
-                m={10}
-                ml={30}
-                src={images[4]}
-                height={180}
-                w={180}
-              />
-            </Flex>
-          </Accordion.Panel>
-        </Accordion.Item>
+            <Accordion.Item className={classes.item} value="another-account">
+              <Accordion.Control>
+                Can I create more that one account?
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Flex>
+                  {placeholder}
+                  <Image
+                    className={classes.faqImage}
+                    m={10}
+                    ml={30}
+                    src={images[0]}
+                    height={220}
+                    w={220}
+                  />
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="credit-card">
-          <Accordion.Control>
-            Do you store credit card information securely?
-          </Accordion.Control>
-          <Accordion.Panel>
-            {" "}
-            <Flex>
-              {placeholder}
-              <Image
-                className={classes.faqImage}
-                m={10}
-                ml={30}
-                src={images[2]}
-                height={180}
-                w={180}
-              />
-            </Flex>
-          </Accordion.Panel>
-        </Accordion.Item>
+            <Accordion.Item className={classes.item} value="newsletter">
+              <Accordion.Control>
+                How can I subscribe to monthly newsletter?
+              </Accordion.Control>
+              <Accordion.Panel>
+                {" "}
+                <Flex>
+                  {placeholder}
+                  <Image
+                    className={classes.faqImage}
+                    m={10}
+                    ml={30}
+                    src={images[4]}
+                    height={220}
+                    w={220}
+                  />
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="payment">
-          <Accordion.Control>
-            What payment systems to you work with?
-          </Accordion.Control>
-          <Accordion.Panel>
-            {" "}
-            <Flex>
-              {placeholder}
-              <Image
-                className={classes.faqImage}
-                m={10}
-                ml={30}
-                src={images[3]}
-                height={180}
-                w={180}
-              />
-            </Flex>
-          </Accordion.Panel>
-        </Accordion.Item>
-      </Accordion>
-    </Container>
+            <Accordion.Item className={classes.item} value="credit-card">
+              <Accordion.Control>
+                Do you store credit card information securely?
+              </Accordion.Control>
+              <Accordion.Panel>
+                {" "}
+                <Flex>
+                  {placeholder}
+                  <Image
+                    className={classes.faqImage}
+                    m={10}
+                    ml={30}
+                    src={images[2]}
+                    height={220}
+                    w={220}
+                  />
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="payment">
+              <Accordion.Control>
+                What payment systems to you work with?
+              </Accordion.Control>
+              <Accordion.Panel>
+                {" "}
+                <Flex>
+                  {placeholder}
+                  <Image
+                    className={classes.faqImage}
+                    m={10}
+                    ml={30}
+                    src={images[3]}
+                    height={220}
+                    w={220}
+                  />
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </Container>
+      </div>
+    </>
   );
 }
